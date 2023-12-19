d2775c440dd5c45dae3b3dc41aef2c7f98459392
Jayden Szeto <Jayden20076757@gmail.com>
2023-12-19 12:16:02 -0800

    a

--- Index/aboutus.js
+++ Index/aboutus.js
@@ -1,14 +1,17 @@
 $(function() {
     $('.profilepic').on('click', function(e) {
         var $biginfo = $('#teamcontent');
+        var $bigimage = $('.bigimg');
         var $bigname = $('#bigname');
         var $bigjob  = $('#bigjob');
         var $bigdesc = $('#bigdesc');
-        
+
+        var newsrc = $(this).attr('src'); 
         var newname = $(this).attr('alt');
         var newrole = $(this).siblings('.job').eq(0).html();
         var newdesc = $(this).siblings('.desc').eq(0).html();
         
+        $bigimage.attr('src', newsrc); 
         $bigname.html(newname);
         $bigjob.html(newrole);
         $bigdesc.html(newdesc);
